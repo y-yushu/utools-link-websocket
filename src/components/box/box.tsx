0a9e771a05ac51a778a45d95aba9bc7ad536612e
yangyushu <yyshu@hisw.cn>
2022-09-05 18:25:50 +0800

    追加更改

--- src/components/box/box.tsx
+++ src/components/box/box.tsx
@@ -1,4 +1,4 @@
-import {useState, useRef} from 'react';
+import {useState} from 'react';
 import {message} from 'antd';
 import YysInput from '../input/input';
 import YysButton from '../button/button';
